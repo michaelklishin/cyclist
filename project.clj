2e606fbaaa626ab31a13c6fe0d19305bbc3e144b
Michael Klishin <michael@clojurewerkz.org>
2014-03-08 14:59:57 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/cyclist "1.0.0"
+(defproject clojurewerkz/cyclist "1.1.0-SNAPSHOT"
   :description "Tiny library that detects cyclic dependencies between any named entities"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/support "0.15.0"]]
