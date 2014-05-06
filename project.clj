5b777db1804c71652c3caed43a37094d015d5b7a
Michael Klishin <michael@clojurewerkz.org>
2014-05-06 14:30:03 -0400

    Depend on Clojure 1.6

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojurewerkz/cyclist "1.1.0-SNAPSHOT"
   :description "Tiny library that detects cyclic dependencies between any named entities"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojurewerkz/support "0.20.0"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
