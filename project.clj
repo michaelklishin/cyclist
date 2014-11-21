36f3370ea423f085e115e70f31bac7cf1daa7a4e
Michael Klishin <michael@clojurewerkz.org>
2014-11-21 18:32:44 +0000

    Drop Clojure 1.4 support

--- project.clj
+++ project.clj
@@ -2,14 +2,13 @@
   :description "Tiny library that detects cyclic dependencies between any named entities"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojurewerkz/support "1.1.0"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
