bd8b64eaf797920f48e50b3b33749638c05c8904
Michael Klishin <michael@clojurewerkz.org>
2014-11-21 18:32:24 +0000

    Bump Support to 1.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojurewerkz/cyclist "1.2.0-SNAPSHOT"
   :description "Tiny library that detects cyclic dependencies between any named entities"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/support "1.0.0"]]
+                 [clojurewerkz/support "1.1.0"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
