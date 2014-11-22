666c97d1425f3449af92afa9f15bf01d11dd57c3
Michael Klishin <michael@clojurewerkz.org>
2014-11-22 05:12:23 +0000

    Test against Clojure 1.7

--- project.clj
+++ project.clj
@@ -2,13 +2,14 @@
   :description "Tiny library that detects cyclic dependencies between any named entities"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojurewerkz/support "1.1.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7    {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
